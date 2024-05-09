8e944ad8d1f06ef0117c3d54c721c81ee394ba15
ySavushkin <elissssey@icloud.com>
2024-05-10 01:19:33 +0200

    Adding user entity

---
+++ src/main/java/com/udemy/spring/springjwt/entities/User.java
@@ -0,0 +1,63 @@
+package com.udemy.spring.springjwt.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data   //getters+setters
+@Builder
+@NoArgsConstructor  //default constructor
+@AllArgsConstructor
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
